48244bfcad68552d4789f31a4da0b432fa884f03
Thais De Macedo <thademacedo@gmail.com>
2023-09-02 11:02:42 -0700

    Created upload.js and upload.scss

---
+++ thais-demacedo-brainflix/src/Upload.js
@@ -0,0 +1,17 @@
+import "./Upload.scss";
+import "./components/Global.scss";
+import Header from "./components/Header";
+
+function Upload() {
+  // const [currentVideo, setCurrentVideo] = useState(data[0]);
+  // console.log(currentVideo);
+  return (
+    <>
+      <div>
+        <Header />
+      </div>
+    </>
+  );
+}
+
+export default Upload;
